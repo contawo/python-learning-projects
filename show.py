a1647d8cea599fd80e98e544bb1a86156b1a7130
Awonke Mnotoza <93478189+Awonke11@users.noreply.github.com>
2024-03-06 11:43:47 +0200

    Float formating

---
+++ show.py
@@ -0,0 +1,18 @@
+'''
+How to fomart
+
+string = %s
+integer = %d
+float = %f
+
+Round of float
+r_float = %.nf
+    n = decimal places you want to round off
+'''
+
+num = 12
+flt = 12.746234234324234
+strin = "Mihle"
+name = "Awonke"
+
+print("String: %s,  Integer: %d,  Float: %.2f" % (strin, num, flt), end=".")
