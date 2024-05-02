3f1a634e710496239aa13cec987deaa8b978ee4b
Awonke Mnotoza <mnotozawonke@gmail.com>
2024-05-02 15:14:20 +0200

    Restored

--- assignment_8b/numberutil.py
+++ assignment_8b/numberutil.py
@@ -1,4 +1,3 @@
-
 word={0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety', 100:'hundred'}
 
 hundred=100
@@ -6,25 +5,6 @@ ten=10
 
 def aswords(n):
     # Returns a textual description of a number in the range 0..999.
-    """
-    >>> aswords(0)
-    'zero'
-    >>> aswords(10)
-    'ten'
-    >>> aswords(25)
-    'twenty five'
-    >>> aswords(20)
-    'twenty'
-    >>> aswords(100)
-    'one hundred'
-    >>> aswords(105)
-    'one hundred and five'
-    >>> aswords(125)
-    'one hundred and twenty five'
-    >>> aswords(130)
-    'one hundred and thirty'
-
-    """
     words = ''
     hundreds = n//hundred
     remainder = n%100
--- assignment_8b/testtimeutil.py
+++ assignment_8b/testtimeutil.py
@@ -1,20 +1,19 @@
-import timeutil 
+"""
+>>> from timeutil import validate
+>>> validate("12")
+False
+>>> validate("hh:00")
+False
+>>> validate("111:00")
+False
+>>> validate("11:00 pm")
+False
+>>> validate("11:m p.m.")
+False
+>>> validate("9:23 p.m.")
+True
 
-# """
-# >>> validate("12")
-# False
-# >>> validate("hh:00")
-# False
-# >>> validate("111:00")
-# False
-# >>> validate("11:00 pm")
-# False
-# >>> validate("11:m p.m.")
-# False
-# >>> validate("9:23 p.m.")
-# True
-
-# """
+"""
 
 import doctest as dt
-dt.testmod(timeutil, verbose=True)
+dt.testmod(verbose=True)
